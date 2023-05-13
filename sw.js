b8a5edcccad8f7ea0829a79134ddb8ad661a0454
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-05-13 09:21:37 +0200

    init - offline css not applied

--- sw.js
+++ sw.js
@@ -1,12 +1,30 @@
-// Listen for the install event
+// Core assets
+let coreAssets = [
+    'offline.html',
+    'offline.jpeg',
+    'favicon.ico',
+    'index.html',
+    'treasure.html',
+    'dice.html',
+    'style.css',
+    'index.js',
+    'treasure.js',
+    'dice.js',
+    
+];
+
+
+// On install, activate immediately
 self.addEventListener('install', function (event) {
 
     // Activate immediately
     self.skipWaiting();
 
-    // Cache the offline.html page
+    // Cache core assets
     event.waitUntil(caches.open('app').then(function (cache) {
-        cache.add(new Request('offline.html'));
+        for (let asset of coreAssets) {
+            cache.add(new Request(asset));
+        }
         return cache;
     }));
 
@@ -28,14 +46,33 @@ self.addEventListener('fetch', function (event) {
     if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
 
     // HTML files
+    // Network-first (If requested html is found serve this else the cached offline.html)
     if (request.headers.get('Accept').includes('text/html')) {
         event.respondWith(
             fetch(request).then(function (response) {
                 return response;
             }).catch(function (error) {
-                return caches.match('offline.html' || '../offline.html');
+                return caches.match(request).then(function (response) {
+                    return response || caches.match('offline.html');
+                });
+            })
+        );
+        return;
+    };
+    
+    // CSS , JavaScript, images
+    // Offline-first (If allready cached take the cached versione, else fetch)
+    if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript') || request.headers.get('Accept').includes('image') ){
+        event.respondWith(
+            caches.match(request).then(function (response) {
+                return response || fetch(request).then(function (response) {
+
+                    // Return the response
+                    return response;
+
+                });
             })
         );
     }
 
-});
+}); 
